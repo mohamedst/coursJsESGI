d8aa328c69c03112549c888b39b1c627cefe10b1
Mohamed <mohamedstrore@hotmail.fr>
2020-09-23 14:28:14 +0200

    feat(game): add startgame function

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import http from 'http';
 import socketIO from 'socket.io';
-import { startGame, terminateGame, handleMove } from './game';
+import { startGame, terminateGame, handleMove } from './game.js';
 
 const server = http.createServer((req, res) => {
     res.write('hello world');
